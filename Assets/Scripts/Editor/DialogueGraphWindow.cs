f91b177ebf925e3952be59818884203410cc8c9a
Dennis <dennischu112@gmail.com>
2024-11-14 11:55:50 +0900

    [DialogueGraphWindow] Add Generate Toolbar

--- Assets/Scripts/Editor/DialogueGraphWindow.cs
+++ Assets/Scripts/Editor/DialogueGraphWindow.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 using UnityEditor;
+using UnityEditor.UIElements;
 
 namespace Dennis.Tools.DialogueGraph
 {
     public class DialogueGraphWindow : EditorWindow
     {
         private DialogueView _dialogueView;
+        private string _fileName = "New Narrative";
 
         [MenuItem("Graph/Dialogue Graph")]
         public static void OpenDialogueGraphWindow()
@@ -19,6 +21,7 @@ namespace Dennis.Tools.DialogueGraph
         private void OnEnable()
         {
             ConstructGraphView();
+            GenerateToolbar();
         }
 
         private void OnDisable()
@@ -38,6 +41,25 @@ namespace Dennis.Tools.DialogueGraph
 
             _dialogueView.StretchToParentSize();
             rootVisualElement.Add(_dialogueView);
+        }
+
+        private void GenerateToolbar()
+        {
+            var toolbar = new Toolbar();
+
+            // Create FileNameText
+            var fileNameTextField = new TextField("File Name :");
+            fileNameTextField.SetValueWithoutNotify(_fileName);
+            fileNameTextField.MarkDirtyRepaint();
+            fileNameTextField.RegisterValueChangedCallback(evt => _fileName = evt.newValue);
+            toolbar.Add(fileNameTextField);
+
+            // Add Save Data Button and Load Data Button
+            // To Do : Button Save / Load function
+            toolbar.Add(new Button() { text = "Save Data" });
+            toolbar.Add(new Button() { text = "Load Data" });
+
+            rootVisualElement.Add(toolbar);
         }
     }
 
