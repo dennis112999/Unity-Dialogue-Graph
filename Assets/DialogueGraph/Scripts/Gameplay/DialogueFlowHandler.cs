5bb891a4db2b134180e890356dd6ff2997809a74
Dennis <98999569+dennis112999@users.noreply.github.com>
2025-02-24 18:49:50 +0900

    [Style] Modify files' location

---
+++ Assets/DialogueGraph/Scripts/Gameplay/DialogueFlowHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using Dennis.Tools.DialogueGraph.Data;
+
+namespace Dennis.Tools.DialogueGraph
+{
+    public class DialogueFlowHandler : MonoBehaviour
+    {
+        // [SerializeField] protected DialogueContainer dialogueContainer;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
